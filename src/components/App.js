b3f011b7be28d1852ff9b363e84a0f1b74a2a4e3
PineGolden311 <software.developer112@gmail.com>
2019-01-13 01:34:47 +0400

    basic structure

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import BoardContainer from './boards/BoardContainer'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <BoardContainer />
+      </div>
+    )
+  }
+}
