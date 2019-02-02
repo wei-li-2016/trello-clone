8c2eed52ba4eae4d8c4a942eb3b5f2b5e8c70358
PineGolden311 <software.developer112@gmail.com>
2019-02-03 02:01:35 +0400

    add cards to list

--- src/Actions/SubmitNewCard.js
+++ src/Actions/SubmitNewCard.js
@@ -1,9 +1,9 @@
 import { SUBMIT_NEW_CARD} from './ActionTypes'
 
-export default function submitNewCard(card) {
+export default function submitNewCard(card, cardId, listId) {
   return dispatch => {
     dispatch({
-      type: SUBMIT_NEW_CARD, payload: card
+      type: SUBMIT_NEW_CARD, payload: { cardName: card, listId, cardId }
     })
   }
 }
--- src/Reducers/ActiveBoardDataReducer.js
+++ src/Reducers/ActiveBoardDataReducer.js
@@ -1,41 +1,36 @@
+import { combineReducers } from 'redux';
 import {
   SUBMIT_LIST,
   SUBMIT_NEW_CARD
 } from './../Actions/ActionTypes'
 import uniqueId from 'lodash/uniqueId'
 
-// const initialState = {
-//   listItem: {
-//     byId: {
-//       id: null,
-//       cards: {
-//         name: [],
-//         id: null
-//       }
-//     }
-//   }
-// }
-
-export default function(state = {}, action) {
+const ListReducer = (state = {}, action) => {
+  const listId = uniqueId("list_");
   switch(action.type) {
     case SUBMIT_LIST:
       return {
         ...state,
-        listItems: {
-          ...state.listItems,
-          [uniqueId('list_')]: {
-            name: action.payload
-          }
+        [listId]: {
+          name: action.payload,
+          id: listId,
+          cards: []
         }
       };
-    // case SUBMIT_NEW_CARD:
-    //   return {
-    //     ...state,
-    //     listItem: {
-    //       cards: state.listItem.cards.concat(action.payload)
-    //     }
-    //   };
+    case SUBMIT_NEW_CARD:
+      const currentList = state[action.payload.listId];
+      currentList.cards.push({name: action.payload.cardName});
+      return {
+        ...state,
+        [action.payload.listId]: currentList,
+      };
     default: 
       return state;
   }
 }
+
+const ActiveBoardReducer = combineReducers({
+  listItems: ListReducer,
+})
+
+export default ActiveBoardReducer;
