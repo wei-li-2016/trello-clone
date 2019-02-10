7935dc031784b9cb2374ab5399bca6a8bd8b0618
PineGolden311 <software.developer112@gmail.com>
2019-02-10 18:50:34 +0400

    change npm script

---
+++ config/jest/cssTransform.js
@@ -0,0 +1,13 @@
+'use strict';
+
+// THis is a custom Jest transformer truning style imports into empty objects.
+
+module.exports = {
+  process() {
+    return 'module.exports = {};';
+  },
+  getCacheKey() {
+    // The output is always the same.
+    return 'cssTransform';
+  },
+};
