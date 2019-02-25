3942ce1b0f095572cd5cfcd5bd1b4f891b028fcf
PineGolden311 <software.developer112@gmail.com>
2019-02-26 01:16:28 +0400

    card container

--- src/components/boards/activeBoard/cards/Card.js
+++ src/components/boards/activeBoard/cards/Card.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types'
-
+import { connect } from 'react-redux'
 import { DragSource } from 'react-dnd';
 import { ItemTypes } from '~Utils/Constants';
+import { fadeIn } from '~Utils/Animations';
+import archiveCard from '~Actions/ArchiveCard';
 
 const CardWrapper = styled.div`
     margin: 10px 0;
@@ -11,6 +13,9 @@ const CardWrapper = styled.div`
     background: rgb(241, 241, 241);
     border-radius: 4.5px;
     cursor: grab;
+    animation: ${fadeIn} 300ms linear;
+    display: flex;
+    justify-content: space-around;
 `
 
 const CardTitle = styled.h3`
@@ -19,50 +24,80 @@ const CardTitle = styled.h3`
     margin: 0;
 `
 
+const ArchiveTask = styled.div`
+    padding: 4px 7px;
+    opacity: 0.4;
+    border: none;
+    border-radius: 9999;
+    cursor: pointer;
+    font-size: 16px;
+`
+
 const cardSource = {
-    beginDrag({ title, cardId, listId }) {
-        return {
-            title, cardId, listId
-        }
+  beginDrag({ title, cardId, listId }) {
+    return {
+      title, cardId, listId
     }
+  }
 }
+
 
 function collect(connect, monitor) {
-	return {
-		connectDragSource: connect.dragSource(),
-		connectDragPreview: connect.dragPreview(),
-		isDragging: monitor.isDragging(),
-	}
+  return {
+    connectDragSource: connect.dragSource(),
+    connectDragPreview: connect.dragPreview(),
+    isDragging: monitor.isDragging(),
+  }
 }
-
 
 @DragSource(ItemTypes.CARD, cardSource, collect)
 class Card extends Component {
 
-	static propTypes = {
-		connectDragSource: PropTypes.func.isRequired,
-		connectDragPreview: PropTypes.func.isRequired,
-		isDragging: PropTypes.bool.isRequired,
-    }
+  static propTypes = {
+    connectDragSource: PropTypes.func.isRequired,
+    connectDragPreview: PropTypes.func.isRequired,
+    isDragging: PropTypes.bool.isRequired,
+  }
 
-    render() {
-        const {
-            isDragging,
-            connectDragSource,
-            title
-        } = this.props;
-        const cardStyles = { opacity: isDragging ? 0.5 : 1 };
+  togglePost = (cardId, listId) => {
+    this.props.archiveCard(cardId, listId)
+  }
 
-        return connectDragSource(
-            <div>
-                <CardWrapper style={cardStyles}>
-                    <CardTitle>{title}</CardTitle>
-                </CardWrapper>
-            </div>
+  render() {
 
-        )
-    }
+    const {
+      isDragging,
+      connectDragSource,
+      title,
+      archiveCard,
+      cardId,
+      listId,
+      isArchived,
+    } = this.props;
+
+    const cardStyles = {
+      opacity: isDragging || isArchived ? 0.35 : 1,
+      boxShadow: "0 6px 6px rgba(0,0,0,0.16), 0 6px 6px rgba(0,0,0,0.23)",
+      textDecoration: isArchived ? "line-through" : "none",
+      backgroundColor: isArchived ? "#DECAFF" : "#caffde",
+    };
+
+    return connectDragSource(
+      <div>
+        <CardWrapper style={cardStyles}>
+          <CardTitle>{title}</CardTitle>
+          <ArchiveTask onClick={() => this.togglePost(cardId, listId)}>✓</ArchiveTask>
+        </CardWrapper>
+      </div>
+
+    )
+  }
 }
 
+const mapStateToProps = ({ activeBoardData }) => {
+  return {
+    activeBoardData
+  }
+}
 
-export default Card;
+export default connect(mapStateToProps, { archiveCard })(Card);
--- src/components/boards/activeBoard/cards/CardContainer.js
+++ src/components/boards/activeBoard/cards/CardContainer.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { Field, reduxForm, reset } from 'redux-form';
 import { connect } from 'react-redux';
-
 import submitNewCard from '~Actions/SubmitNewCard';
 import BoardTitleInput from './../../boardCreation/BoardTitleInput';
 import Card from './Card';
@@ -10,19 +9,21 @@ import uniqueId from 'lodash/uniqueId';
 class CreateCardContainer extends Component {
 
   submit = values => {
-    const { listId } = this.props;
-    let cardName = `cardName_${listId}`
-    this.props.submitNewCard(values[cardName], uniqueId('cardItem_'), listId)
+    const { listId, submitNewCard } = this.props;
+    let cardName = `cardName_${listId}`;
+    submitNewCard(values[cardName], uniqueId('cardItem_'), listId);
   }
 
   renderCards = () => {
-    const { activeBoardData, listId, archiveCard } = this.props;
+    const { activeBoardData, listId } = this.props;
     return activeBoardData.listItems[listId].cards.map((card, i) => {
       return (
         <Card
           key={i}
           title={card.name}
+          cardId={card.cardId}
           listId={card.listId}
+          isArchived={card.isArchived}
         />
       )
     })
@@ -47,20 +48,18 @@ class CreateCardContainer extends Component {
   }
 }
 
-const validate = (values, props) => {
+function validate(values) {
   const errors = {};
-  const { listId } = props;
-  let cardName = `cardName_${listId}`
 
-  if (!values[cardName]) {
-    errors[cardName] = 'oops, give me name';
+  if (!values.cardName) {
+    errors.cardName = 'oops!';
   }
 
   return errors;
 }
 
 const afterSubmit = (result, dispatch) => {
-  dispatch(reset('card'));
+  dispatch(reset('cardName'));
 }
 
 function mapStateToProps({ activeBoardData }) {
@@ -69,6 +68,6 @@ function mapStateToProps({ activeBoardData }) {
 
 export default reduxForm({
   validate,
-  form: 'card',
+  form: 'cardName',
   onSubmitSuccess: afterSubmit,
-})(connect(mapStateToProps, { submitNewCard, archiveCard })(CreateCardContainer));
+})(connect(mapStateToProps, { submitNewCard })(CreateCardContainer));
